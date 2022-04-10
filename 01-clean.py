fd0f45d597844247194a36757984a371ee3a5f40
stevemichaelwhite <stevemichaelwhite@gmail.com>
2022-04-10 18:57:23 +1200

    added pipeline

--- 01-clean.py
+++ 01-clean.py
@@ -25,10 +25,56 @@ categorical_cols = [cname for cname in X_train_full.columns if
 numerical_cols = [cname for cname in X_train_full.columns if 
                 X_train_full[cname].dtype in ['int64', 'float64']]
 
-# Keep selected columns only
+# Keep selected columns only 
 keep_cols = categorical_cols + numerical_cols
 X_train = X_train_full[keep_cols].copy()
 X_valid = X_valid_full[keep_cols].copy()
 X_test = X_test_full[keep_cols].copy()
+
+# Pipeline ###################################
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='median')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Bundle preprocessing and modeling code in a pipeline
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('model', model)
+                     ])
+
+# Preprocessing of training data, fit model 
+clf.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = clf.predict(X_valid)
+
+print('MAE:', mean_absolute_error(y_valid, preds))
+
+preds_test = clf.predict(X_test) 
+
+output = pd.DataFrame({'Id': X_test.index,
+                       'SalePrice': preds_test})
+output.to_csv('data/output/submission.csv', index=False)
 
 

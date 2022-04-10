1bf5d633eb3d1b5cbf0af972547a094eb26763ad
stevemichaelwhite <stevemichaelwhite@gmail.com>
2022-04-10 18:57:10 +1200

    Initial commit

---
+++ 01-clean.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('data/input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('data/input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X_full, y, 
+                                                                train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if
+                    X_train_full[cname].nunique() < 10 and 
+                    X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if 
+                X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+keep_cols = categorical_cols + numerical_cols
+X_train = X_train_full[keep_cols].copy()
+X_valid = X_valid_full[keep_cols].copy()
+X_test = X_test_full[keep_cols].copy()
+
+
